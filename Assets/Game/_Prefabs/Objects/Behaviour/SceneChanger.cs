e1a263c2785b42384a4ad9b621bd6b4bc50f498c
That0neDev <sanlikaaneren@gmail.com>
2024-01-22 20:26:19 +0300

    son hali

---
+++ Assets/Game/_Prefabs/Objects/Behaviour/SceneChanger.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    public SpriteRenderer conirmation;
+    public int sceneID;
+
+    public bool canUse;
+
+    private void Open()
+    {
+        conirmation.enabled = true;
+        canUse = true;
+    }
+    private void Close()
+    {
+        conirmation.enabled = false;
+        canUse = false;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform == player)
+            Open();
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform == player)
+            Close();
+    }
+
+    public void OnDoorInput(InputAction.CallbackContext ctx)
+    {
+        if (canUse && ctx.phase == InputActionPhase.Started)
+            SceneManager.LoadScene(sceneID);
+    }
+}
